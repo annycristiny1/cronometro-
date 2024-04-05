055036a92a17bb52612438100051b737874c88f5
annycristiny1 <111583213+annycristiny1@users.noreply.github.com>
2024-04-05 11:21:05 -0300

    vcf

--- main.js
+++ main.js
@@ -16,9 +16,14 @@ for (let i = 0; i < botoes.length; i++) {
 
 const contadores = document.querySelectorAll(".contador");
 const tempoObjetivo1 = new Date("2024-10-05T00:00:00");
+const tempoObjetivo3 = new Date("2023-12-30T00:00:00");
+const tempoObjetivo4 = new Date("2024-02-01T00:00:00");
 let tempoAtual = new Date();
 
-contadores[0].textContent = calculaTempo(tempoObjetivo1);
+const tempos = [tempoObjetivo1, tempoObjetivo2, tempoObjetivo3, tempoObjetivo4];
+
+contadores[0].textContent = calculaTempo(tempos[0]);
+contadores[1].textContent = calculaTempo(tempoObjetivo2);
 
 function calculaTempo(tempoObjetivo) {
     let tempoAtual = new Date();
